3de6dc7b50cd2eb5791bfcbd162baff9a2460e01
oumaima <81293137+Souguiroumaima7@users.noreply.github.com>
2021-12-18 20:24:14 +0100

    Add files via upload

---
+++ views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.utils import timezone
+from blog.models import Post
+from django.shortcuts import render
+
+def index(request):
+    posts = Post.objects.filter(published_at__lte=timezone.now())
+    return render(request, "blog/index.html", {"posts": posts})
