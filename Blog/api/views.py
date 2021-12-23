0adf42628152803d486e4b88144d5a0d17e2f31c
oumaima <81293137+Souguiroumaima7@users.noreply.github.com>
2021-12-23 15:15:52 +0100

    Add files via upload

--- Blog/api/views.py
+++ Blog/api/views.py
@@ -1,12 +1,14 @@
+from django.contrib.auth.models import User
 from rest_framework import generics
 
 import blog.api.serializers
 import blog.models
 
 from Blog.api.permissions import AuthorModifyOrReadOnly , IsAdminUserForObject
-from Blog.api.serializers import PostSerializer
+from Blog.api.serializers import PostSerializer , UserSerializer , PostDetailSerializer
 from Blog.models import Post
 import blog.api.permissions
+
 
 
 class PostList(generics.ListCreateAPIView):
@@ -23,4 +25,15 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [AuthorModifyOrReadOnly]
     # leave other attributes as is
 permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
+
+class UserDetail(generics.RetrieveAPIView):
+    lookup_field = "email"
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+import blog.api.serializers
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
+    queryset = Post.objects.all()
+    serializer_class = PostDetailSerializer
 
