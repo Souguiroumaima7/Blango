00f43945cd11745be2cf56bca6dcac72f0f0bc0e
oumaima <81293137+Souguiroumaima7@users.noreply.github.com>
2021-12-24 11:41:52 +0100

    Add files via upload

--- urls.py
+++ urls.py
@@ -13,11 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import blog
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-  path("post/<slug>/",blog.views.post_detail,name="blog-post_detail")
-  
-]
+    ]
+import debug_toolbar
+from django.conf import settings
+from django.urls import path, include
+
+if settings.DEBUG:
+    urlpatterns += [
+        path("__debug__/", include(debug_toolbar.urls)),
+    ]
