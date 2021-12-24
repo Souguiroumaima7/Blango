46290d7569615b252a6b14d14b91552f1264a764
oumaima <81293137+Souguiroumaima7@users.noreply.github.com>
2021-12-24 11:40:54 +0100

    Add files via upload

--- templates/templatetags/blog_extras.py
+++ templates/templatetags/blog_extras.py
@@ -1,9 +1,7 @@
 from django.contrib.auth import get_user_model
 from django import template
-from django.utils.html import escape
+from django.utils.html import escape, format_html
 from django.utils.safestring import mark_safe
-from blog.models import Post 
-
 
 user_model = get_user_model()
 register = template.Library()
@@ -30,17 +28,22 @@ def author_details(author):
 
     return f"{prefix}{name}{suffix}"
 
-  def row () : 
+
+def row():
     return '<div class = "row">'
-  
-  @register.simple_tag 
-  def endrow(): 
-    return "</div>" 
-  
-  @register.simple_tag 
-  def row(extra_classes=""): 
-    return format_html('<div class="row {}">',extra_classes)
-  @register.simple_tag
+
+
+@register.simple_tag
+def endrow():
+    return "</div>"
+
+
+@register.simple_tag
+def row(extra_classes=""):
+    return format_html('<div class="row {}">', extra_classes)
+
+
+@register.simple_tag
 def col(extra_classes=""):
     return format_html('<div class="col {}">', extra_classes)
 
@@ -48,9 +51,26 @@ def col(extra_classes=""):
 @register.simple_tag
 def endcol():
     return format_html("</div>")
+@register.simple_tag(takes_context=True)
+def author_details_tag(context):
+    request = context["request"]
+    current_user = request.user
+    post = context["post"]
+    author = post.author
 
-  @register.inclusion_tag("blog/post-list.html")
-  
-  def recent_posts(post) : 
-    posts = posts.objects.exclude(pk=post.pk)[:5] 
-    return {"title": "recent posts","posts":posts }
+    if author == current_user:
+        return format_html("<strong>me</strong>")
+
+    if author.first_name and author.last_name:
+        name = f"{author.first_name} {author.last_name}"
+    else:
+        name = f"{author.username}"
+
+    if author.email:
+        prefix = format_html('<a href="mailto:{}">', author.email)
+        suffix = format_html("</a>")
+    else:
+        prefix = ""
+        suffix = ""
+
+    return format_html("{}{}{}", prefix, name, suffix)
