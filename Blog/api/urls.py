11e272b78b07b3982ff93bd17a1fac745eabed97
oumaima <81293137+Souguiroumaima7@users.noreply.github.com>
2021-12-24 11:40:22 +0100

    Add files via upload

--- Blog/api/urls.py
+++ Blog/api/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path , include
+from django.urls import path , include , re_path
+from drf_yasg2 import openapi
+from drf_yasg2.views import get_schema_view
 from rest_framework.urlpatterns import format_suffix_patterns
-
-import blog.api.views
 
 from Blog.api.api_views import PostDetail
 from Blog.api.views import PostList , UserDetail
@@ -27,3 +27,29 @@ urlpatterns = [
     path("users/<str:email>", UserDetail.as_view(), name="api_user_detail"),
 ]
 
+import drf_yasg.views
+import os
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Blango API",
+        default_version="v1",
+        description="API for Blango Blog",
+    ),
+    url=f"https://{os.environ.get('CODIO_HOSTNAME')}-8000.codio.io/api/v1/",
+    public=True,
+)
+
+urlpatterns += [
+    path("auth/", include("rest_framework.urls")),
+    path("token-auth/", views.obtain_auth_token),
+    re_path(
+        r"^swagger(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+]
