7c8ba86373c0fac6262cf20ffe42d3f520b2b518
oumaima <81293137+Souguiroumaima7@users.noreply.github.com>
2021-12-25 19:21:49 +0100

    Add files via upload

--- Blog/api/urls.py
+++ Blog/api/urls.py
@@ -1,10 +1,12 @@
+import debug_toolbar
+from django.conf import settings
 from django.urls import path , include , re_path
 from drf_yasg2 import openapi
 from drf_yasg2.views import get_schema_view
 from rest_framework.urlpatterns import format_suffix_patterns
-
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from Blog.api.api_views import PostDetail
-from Blog.api.views import PostList , UserDetail
+from Blog.api.views import PostList , UserDetail , TagViewSet , PostViewSet
 
 urlpatterns = [
     path("posts/", PostList.as_view(), name="api_post_list"),
@@ -53,3 +55,36 @@ urlpatterns += [
         name="schema-swagger-ui",
     ),
 ]
+from rest_framework.routers import DefaultRouter
+import blog.api.views
+router = DefaultRouter()
+router.register("tags", TagViewSet)
+
+urlpatterns += [
+    path("auth/", include("rest_framework.urls")),
+    # ... other patterns omitted
+    path("", include(router.urls)),
+]
+
+path("posts/" , PostList.as_view() , name="api_post_list") ,
+path("posts/<int:pk>" , PostDetail.as_view() , name="api_post_detail") ,
+urlpatterns = [
+    path("users/<str:email>", UserDetail.as_view(), name="api_user_detail"),
+]
+router.register("posts", PostViewSet)
+
+path(
+    "posts/by-time/<str:period_name>/" ,
+    PostViewSet.as_view({"get" : "list"}) ,
+    name="posts-by-time" ,
+) ,
+path("", include(router.urls)),
+
+path("jwt/" , TokenObtainPairView.as_view() , name="jwt_obtain_pair") ,
+path("jwt/refresh/" , TokenRefreshView.as_view() , name="jwt_refresh") ,
+
+from django.conf.urls.static import static
+if settings.DEBUG:
+    urlpatterns += [
+        path("__debug__/", include(debug_toolbar.urls)),
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
